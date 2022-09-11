497f76f476caae69b6283c38edb3a395bdb5d18f
Syed Aabis Abbas Zaidi <71585201+AabisZ@users.noreply.github.com>
2022-09-11 14:55:34 +0500

    Counter App
    Bootcamp session 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  let [count,setCount]=useState(0);
+  let [isMorning,setMorning]=useState(true);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${isMorning ? 'dayLight' :'App'}`}>
+      <h1>Hello World</h1>
+      <h1>Day Time = {isMorning ? 'Morning':'Night'}</h1>
+      <button onClick={()=> setMorning(!isMorning)}>Change clr</button>
+
+      <h3>Value of counter variable is: {count}</h3>
+      <button onClick={
+        () => setCount(++count) }>Update counter</button>
+      <br></br>
+      <button onClick={() => setCount(0)}>Reset</button>
+
     </div>
   );
 }
